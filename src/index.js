e1c2140f0ddd5cce832ef42759b048bc33a111dd
melvinkukwansafari <melvin.kukwan@student.moringaschol.com>
2023-01-26 20:32:45 +0300

    one test passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter, Route, } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 function Home() {
   return (
@@ -9,8 +12,53 @@ function Home() {
   );
 }
 
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
 function App() {
   return <Home />;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<App />,
+<BrowserRouter>
+  <Switch>
+    
+    
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route exact path="/">
+      <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  
+
+document.getElementById("root")
+);
